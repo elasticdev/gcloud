bd89ab3b37ccdbbbe540fb164635d5747874ea84
Gary <gary@elasticdev.io>
2020-08-05 19:41:13 +0800

    update

--- stacks/_ed_configs/gcloud_simple_vpc/_main/run.py
+++ stacks/_ed_configs/gcloud_simple_vpc/_main/run.py
@@ -13,8 +13,8 @@ def run(stackargs):
     # this will set the GOOGLE_APPLICATION_CREDENTIALS environment variable relative to the shared docker volume
     # it should be in the directory /var/tmp/terraform
     stack.parse.add_required(key="google_application_credentials",default="/var/tmp/share/.creds/gcloud.json")
-    stack.parse.add_required(key="auto_create_subnetworks",default="false")
-    stack.parse.add_required(key="global_address_block",default="false")
+    stack.parse.add_required(key="auto_create_subnetworks",default="null")
+    stack.parse.add_required(key="global_address_block",default="null")
 
     # docker image to execute terraform with
     stack.parse.add_optional(key="gcloud_region",default="us-west1")
@@ -37,21 +37,32 @@ def run(stackargs):
     # initialize exegroups for introspection and dependencies
     stack.init_execgroups()
 
+    null_values = [ None, "None", False, "false"]
+
     # CREATE EMPTY VPC
     vpc_state_id = stack.random_id(size=8)
 
     env_vars = {"NAME":stack.vpc_name}
-    env_vars["VPC_NAME"] = stack.vpc_name
+
     env_vars["GCLOUD_PROJECT"] = stack.gcloud_project
+    env_vars["TF_VAR_gcloud_project"] = stack.gcloud_project
+
+    env_vars["VPC_NAME"] = stack.vpc_name
+    env_vars["TF_VAR_vpc_name"] = stack.vpc_name
+
     env_vars["GOOGLE_APPLICATION_CREDENTIALS"] = stack.google_application_credentials
     env_vars["DOCKER_EXEC_ENV"] = stack.docker_exec_env
-    env_vars["AUTO_CREATE_SUBNETWORKS"] = stack.auto_create_subnetworks
     env_vars["USE_DOCKER"] = True
     env_vars["METHOD"] = "create"
 
     env_vars["TERRAFORM_RESOURCE_TYPE"] = "google_compute_network"
     env_vars["RESOURCE_TYPE"] = "vpc"
     env_vars["RESOURCE_TAGS"] = [ "vpc", stack.vpc_name ]
+
+    if stack.auto_create_subnetworks not in null_values:
+        env_vars["AUTO_CREATE_SUBNETWORKS"] = "true"
+        env_vars["TF_VAR_auto_create_subnetworks"] = "true"
+
     #env_vars["RESOURCE_PARENT"] = True
 
     # determine what env vars to pass to 
@@ -68,21 +79,31 @@ def run(stackargs):
 
     # if auto create subnets is not True, then we need to 
     # explicity specify the subnets
-
-    null_values = [ None, "None", False, "false"]
-
     # CREATE SUBNETS
     if stack.auto_create_subnetworks in null_values:
 
         subnet_state_id = stack.random_id(size=8)
 
         env_vars = {"NAME":subnet_state_id}
-        env_vars["VPC_NAME"] = stack.vpc_name
-        env_vars["GCLOUD_PROJECT"] = stack.gcloud_project
-        env_vars["GCLOUD_REGION"] = stack.gcloud_region
-        env_vars["PUBLIC_CIDR"] = stack.public_cidr
-        env_vars["PRIVATE_CIDR"] = stack.private_cidr
         env_vars["STATEFUL_ID"] = subnet_state_id
+
+        env_vars["GCLOUD_PROJECT"] = stack.gcloud_project
+        env_vars["TF_VAR_gcloud_project"] = stack.gcloud_project
+
+        env_vars["VPC_NAME"] = stack.vpc_name
+        env_vars["TF_VAR_vpc_name"] = stack.vpc_name
+
+        env_vars["GCLOUD_REGION"] = stack.gcloud_region
+        env_vars["TF_VAR_gcloud_region"] = stack.gcloud_region
+
+        env_vars["PUBLIC_CIDR"] = stack.public_cidr
+        env_vars["TF_VAR_public_cidr"] = stack.public_cidr
+
+        env_vars["PRIVATE_CIDR"] = stack.private_cidr
+        env_vars["TF_VAR_private_cidr"] = stack.private_cidr
+
+        env_vars["AUTO_CREATE_SUBNETWORKS"] = "false"
+        env_vars["TF_VAR_auto_create_subnetworks"] = "false"
 
         env_vars["GOOGLE_APPLICATION_CREDENTIALS"] = stack.google_application_credentials
         env_vars["DOCKER_EXEC_ENV"] = stack.docker_exec_env
@@ -120,11 +141,18 @@ def run(stackargs):
     firewall_state_id = stack.random_id(size=8)
 
     env_vars = {"NAME":firewall_state_id}
+
     env_vars["VPC_NAME"] = stack.vpc_name
+    env_vars["TF_VAR_vpc_name"] = stack.vpc_name
+
     env_vars["GCLOUD_PROJECT"] = stack.gcloud_project
+    env_vars["TF_VAR_gcloud_project"] = stack.gcloud_project
+
     env_vars["PUBLIC_CIDR"] = stack.public_cidr
+    env_vars["TF_VAR_public_cidr"] = stack.public_cidr
+
     env_vars["PRIVATE_CIDR"] = stack.private_cidr
-    env_vars["STATEFUL_ID"] = firewall_state_id
+    env_vars["TF_VAR_private_cidr"] = stack.private_cidr
 
     env_vars["GOOGLE_APPLICATION_CREDENTIALS"] = stack.google_application_credentials
     env_vars["DOCKER_EXEC_ENV"] = stack.docker_exec_env

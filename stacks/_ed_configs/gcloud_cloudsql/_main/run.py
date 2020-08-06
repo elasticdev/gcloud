82870e5bc2aafed67ad44fa7e5a325e04f6d2307
Gary <gary@elasticdev.io>
2020-08-06 12:32:40 +0800

    update

--- stacks/_ed_configs/gcloud_cloudsql/_main/run.py
+++ stacks/_ed_configs/gcloud_cloudsql/_main/run.py
@@ -22,9 +22,7 @@ def run(stackargs):
     stack.parse.add_optional(key="docker_exec_env",default="elasticdev/terraform-run-env")
 
     stack.add_execgroup("elasticdev:::gcloud::base elasticdev:::gcloud::cloudsql","cloudsql")
-
-    # Testingyoyo
-    #stack.add_substack('elasticdev:::publish_cloudsql_info')
+    stack.add_substack('elasticdev:::publish_cloudsql_info')
 
     # initialize variables
     stack.init_variables()
